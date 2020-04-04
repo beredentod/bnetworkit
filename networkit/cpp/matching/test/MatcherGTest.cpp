10d1c784e020dcdb7cee9d3efb5a4450bb026900
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2020-04-04 02:34:11 +0200

    New test for suitor

--- networkit/cpp/matching/test/MatcherGTest.cpp
+++ networkit/cpp/matching/test/MatcherGTest.cpp
@@ -7,25 +7,42 @@
 
 #include <gtest/gtest.h>
 
+#include <networkit/auxiliary/Random.hpp>
+#include <networkit/graph/Graph.hpp>
+#include <networkit/graph/GraphTools.hpp>
+#include <networkit/io/DibapGraphReader.hpp>
+#include <networkit/io/METISGraphReader.hpp>
+#include <networkit/io/NetworkitBinaryReader.hpp>
+#include <networkit/matching/LocalMaxMatcher.hpp>
 #include <networkit/matching/Matcher.hpp>
 #include <networkit/matching/Matching.hpp>
 #include <networkit/matching/PathGrowingMatcher.hpp>
-#include <networkit/matching/LocalMaxMatcher.hpp>
-#include <networkit/graph/Graph.hpp>
-#include <networkit/io/DibapGraphReader.hpp>
-#include <networkit/io/METISGraphReader.hpp>
-#include <networkit/auxiliary/Random.hpp>
+#include <networkit/matching/SuitorMatcher.hpp>
 
 namespace NetworKit {
 
-class MatcherGTest: public testing::Test {};
+class MatcherGTest : public testing::Test {};
+
+TEST_F(MatcherGTest, testSuitorMatcher) {
+    Graph G(4, true, false);
+    G.addEdge(0, 1, 6);
+    G.addEdge(0, 2, 5);
+    G.addEdge(1, 2, 8);
+    G.addEdge(1, 3, 3);
+    G.addEdge(2, 3, 11);
+    //G = NetworkitBinaryReader{}.read("/home/angriman/graphs/advogato.nkb");
+    //if (!G.isWeighted())
+    //    G = GraphTools::toWeighted(G);
+    //G.forEdges([&](node u, node v) { G.setWeight(u, v, Aux::Random::probability()); });
+    G.sortOutEdgesByWeight(std::greater<edgeweight>());
+    SuitorMatcher sm(G);
+    sm.run();
+}
 
 TEST_F(MatcherGTest, testLocalMaxMatching) {
     count n = 50;
     Graph G(n);
-    G.forNodePairs([&](node u, node v){
-        G.addEdge(u,v);
-    });
+    G.forNodePairs([&](node u, node v) { G.addEdge(u, v); });
 
     LocalMaxMatcher localMaxMatcher(G);
 
@@ -47,22 +64,20 @@ TEST_F(MatcherGTest, testLocalMaxMatching) {
     M = lmm.getMatching();
     isProper = M.isProper(airfoil1);
     EXPECT_TRUE(isProper);
-    DEBUG("LocalMax on airfoil1 produces matching of size: " , M.size(G));
+    DEBUG("LocalMax on airfoil1 produces matching of size: ", M.size(G));
 #endif
 }
 
 TEST_F(MatcherGTest, testLocalMaxMatchingDirectedWarning) {
     Graph G(2, false, true);
-    G.addEdge(0,1);
+    G.addEdge(0, 1);
     EXPECT_THROW(LocalMaxMatcher localMaxMatcher(G), std::runtime_error);
 }
 
 TEST_F(MatcherGTest, testPgaMatchingOnWeightedGraph) {
     count n = 50;
     Graph G(n);
-    G.forNodePairs([&](node u, node v){
-        G.addEdge(u,v, Aux::Random::real());
-    });
+    G.forNodePairs([&](node u, node v) { G.addEdge(u, v, Aux::Random::real()); });
     PathGrowingMatcher pgaMatcher(G);
     EXPECT_NO_THROW(pgaMatcher.run());
 }
@@ -70,22 +85,15 @@ TEST_F(MatcherGTest, testPgaMatchingOnWeightedGraph) {
 TEST_F(MatcherGTest, testPgaMatchingWithSelfLoops) {
     count n = 50;
     Graph G(n);
-    G.forNodePairs([&](node u, node v){
-        G.addEdge(u,v, Aux::Random::real());
-    });
-    G.forNodes([&](node u){
-        G.addEdge(u,u);
-    });
-    EXPECT_THROW(PathGrowingMatcher pgaMatcher(G),std::invalid_argument);
+    G.forNodePairs([&](node u, node v) { G.addEdge(u, v, Aux::Random::real()); });
+    G.forNodes([&](node u) { G.addEdge(u, u); });
+    EXPECT_THROW(PathGrowingMatcher pgaMatcher(G), std::invalid_argument);
 }
-
 
 TEST_F(MatcherGTest, testPgaMatching) {
     count n = 50;
     Graph G(n);
-    G.forNodePairs([&](node u, node v){
-        G.addEdge(u,v);
-    });
+    G.forNodePairs([&](node u, node v) { G.addEdge(u, v); });
     PathGrowingMatcher pgaMatcher(G);
 
     DEBUG("Start PGA matching on 50-clique");
@@ -99,7 +107,6 @@ TEST_F(MatcherGTest, testPgaMatching) {
     EXPECT_EQ(M.size(G), numExpEdges);
     DEBUG("Finished PGA matching on 50-clique");
 
-
 #if !defined _WIN32 && !defined _WIN64 && !defined WIN32 && !defined WIN64
     DibapGraphReader reader;
     Graph airfoil1 = reader.read("input/airfoil1.gi");
@@ -108,7 +115,7 @@ TEST_F(MatcherGTest, testPgaMatching) {
     M = pga2.getMatching();
     isProper = M.isProper(airfoil1);
     EXPECT_TRUE(isProper);
-    DEBUG("PGA on airfoil1 produces matching of size: " , M.size(G));
+    DEBUG("PGA on airfoil1 produces matching of size: ", M.size(G));
 #endif
 }
 
